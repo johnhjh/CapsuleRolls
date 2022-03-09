33cb083dacd5a1a923d3d70c65ebab60bc2a65a5
Hyun Ho "John" An <john_an@nate.com>
2022-03-10 00:00:34 +0900

    22.03.10 PlayerData On Work

---
+++ Assets/01.Scripts/Entity/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Capsule.Entity
+{
+    public class PlayerData
+    {
+
+    }
+}
